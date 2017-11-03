91682e350717aaea206dbee2a1e279d3b8192c5c
js <jonathanstein@live.com>
2017-11-03 11:03:41 -0700

    add basic API call to get all starred repos

--- PersonalSite/Controllers/HomeController.cs
+++ PersonalSite/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using PersonalSite.Models;
 namespace PersonalSite.Controllers
 {
     public class HomeController : Controller
@@ -15,16 +15,15 @@ namespace PersonalSite.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            
 
             return View();
         }
 
-        public IActionResult Contact()
+        public IActionResult Projects()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var starredProjects = Project.GetProjects();
+            return View(starredProjects);
         }
 
         public IActionResult Error()
---
+++ PersonalSite/Models/Project.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+namespace PersonalSite.Models
+{
+    public class Project
+    {
+        public string full_name { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string Stars { get; set; }
+        public string Language { get; set; }
+        public DateTime LastUpdated {get;set;}
+
+        public static List<Project> GetProjects()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.github.com/users/jstein92/starred");
+            client.AddDefaultHeader("User-Agent", "Jstein92");
+            var request = new RestRequest();
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            var messageList = JsonConvert.DeserializeObject<List<Project>>(jsonResponse.ToString());
+            return messageList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+
+  
+}
